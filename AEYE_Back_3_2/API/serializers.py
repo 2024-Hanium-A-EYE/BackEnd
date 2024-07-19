f717a52a19ca702bcbb762c6722977761e0a343d
Yoonchul <yoonchul005@gmail.com>
2024-07-19 21:04:36 +0900

    Intialize

--- AEYE_Back_3_2/API/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import getRequestModel
-
-class RequestSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = getRequestModel
-        fields = '__all__'
