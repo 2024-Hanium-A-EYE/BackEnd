3589eaf36ce78f13a574487acb4637cb43a94887
yoonchulchung <yoonchul005@gmail.com>
2024-07-30 16:19:44 +0900

    updated

--- WEB_automate.sh
+++ WEB_automate.sh
@@ -9,7 +9,7 @@ NC='\033[0m' # No Color (Defualt colour)
 ##################################################################
 install_status_VAR=1
 
-total_progress=2
+total_progress=4
 
 print_AEYE_WEB() {
   clear
@@ -46,80 +46,80 @@ initialize_system
 
 ########################################################################
 
-intialize_docker_install () {
-  sudo apt install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+uninstall_docker() {
+  sudo systemctl stop docker.socket
+  sudo systemctl disable docker.socket
+  sudo systemctl stop docker.service
+
+  dpkg -l | grep -i docker 
+  sudo apt-get purge -y docker-*
+  sudo apt-get purge -y python3-docker*
   
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg -y --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  
-  echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
-  if [ $? -eq 0 ]; then
-    echo -e "${BLUE}[1/$total_progress] ${NC}Docker install initialization Succeed."
-  else
-    install_status_VAR=1
-    echo -e "${RED}[1/$total_progress] ${NC}Dokcer install initialization Failed."
-  fi
+  dpkg -l | grep -i docker 
+
+  sudo rm -rf /var/lib/docker* /etc/docker*
+  sudo rm -rf /var/run/docker.sock /var/run/dockershim.sock /var/run/docker.pid
+  sudo rm -rf /var/lib/dockershim /var/run/docker /var/run/dockershim.sock
+  sudo apt purge -y docker-ce docker-ce-cli containerd.io
+
 }
 
 install_docker () {
-  intialize_docker_install
   
-  sudo apt update
-  if [ $? -eq 0 ]; then
-    sudo apt install -y docker-ce docker-ce-cli containerd.io
+  sudo apt-get update
+  sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update
     
-    # Create the docker group.
-    sudo groupadd docker
+  if [ $? -eq 0 ]; then
+    
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io
   else
     install_status_VAR=1
-    echo -e "${RED}[1/$total_progress] ${NC}Failed to install docker"
+    echo -e "${RED}[2/$total_progress] ${NC}Failed to install docker"
   fi
   
-}
-
-install_nvidia_docker() {
-  distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-    && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
-    && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-   
-  sudo apt update
-  sudo apt install -y nvidia-docker2
-
-  sudo systemctl restart docker
 }
 
 install() {
 
   if [ $install_status_VAR -eq 0 ]; then
-    echo -e "${BLUE}[1/$total_progress] ${NC}Install Docker..."
+    echo -e "${BLUE}[1/$total_progress] ${NC}Uninstall Docker..."
+    figlet Uninstall Docker
+    uninstall_docker
+  else
+    echo -e "${RED}[1/$total_progress] ${NC}Failed to uninstall Docker due to install_status_VAR = 1"
+  fi
+
+  if [ $install_status_VAR -eq 0 ]; then
+    echo -e "${BLUE}[2/$total_progress] ${NC}Install Docker..."
     figlet Install Docker
     install_docker 
   else
-    echo -e "${RED}[1/$total_progress] ${NC}Failed to install Docker due to install_status_VAR = 1"
+    echo -e "${RED}[2/$total_progress] ${NC}Failed to install Docker due to install_status_VAR = 1"
   fi
     
   if [ $install_status_VAR -eq 0 ]; then
-    echo -e "${BLUE}[2/$total_progress] ${NC}Install Nvidia Doocker...."
+    echo -e "${BLUE}[3/$total_progress] ${NC}Install Docker Compose...."
     figlet Install 
-    figlet Nvidia Docker
-    install_nvidia_docker
+    figlet Docker Compose
+    sudo apt install docker-compose -y
   else
-    echo -e "${RED}[2/$total_progress] ${NC}Failed to install Nvidia Docker due to install_status_VAR = 1"
+    echo -e "${RED}[3/$total_progress] ${NC}Failed to install Docker Compose due to install_status_VAR = 1"
   fi
 }
 
 install
 
-# run server
-#figlet Start Server
-#cd AEYE_AI_Back_3_2
-#python manage.py runserver
+run_server() {
+  if [ $install_status_VAR -eq 0 ]; then
+    echo -e "${BLUE}[4/$total_progress] ${NC}Run AEYE Web Server"
+    cd Docker && sudo docker-compose up
+  else
+    echo -e "${RED}[4/$total_progress] ${NC}Failed to Run AEYE Web Server due to install_status_VAR = 1"
+  fi
+  
+}
 
-
-
+run_server
