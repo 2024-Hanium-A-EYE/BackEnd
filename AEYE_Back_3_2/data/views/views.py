2d4b1ab721bcf83adb2e2079aa4fa1ea2005ec2a
yoonchulchung <yoonchul005@gmail.com>
2024-08-03 19:19:43 +0900

    Fixed Docker

--- AEYE_Back_3_2/data/views/views.py
+++ AEYE_Back_3_2/data/views/views.py
@@ -6,9 +6,5 @@ from rest_framework import status
 import requests
 import os, sys
 from . import log
+from . import ip
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-root_dir = os.path.abspath(os.path.join(current_dir, os.pardir, os.pardir, os.pardir))
-sys.path.append(root_dir)
-
-import ip
